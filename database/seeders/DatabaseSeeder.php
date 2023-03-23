dcae1688068c08ee7834a3da6db9c318971b7e6c
su apt install kuma <maxxozou@gmail.com>
2023-03-23 14:35:27 +0100

    Ver 1.0

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Post;
+use App\Models\User;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +15,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        //Créer 5 catégories
+        $categories = Category::factory(5)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        //Créer 10 user
+        User::factory(10)->create()->each(function ($user) use ($categories){
+
+            //Pour chaque user il créer 1 à 3 posts
+            Post::factory(rand(1, 3))->create([
+
+                //Créer le user_id et la catégorie
+                'user_id' => $user->id,
+                'category_id' => ($categories->random(1)->first())->id
+            ]);
+        });
     }
 }
